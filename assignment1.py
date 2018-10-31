b2b2760df717d863bdb46b623346af74185e1804
yuya737 <kawakami@grinnell.edu>
2018-10-31 12:50:58 +0000

    Fixing again...

--- assignment1.py
+++ assignment1.py
@@ -5,12 +5,47 @@ import math
 
 
 def calculateDistance(point1, point2, dimension):
+    """Short summary.
+        Calculate Distance between point1 and point2
+
+    Parameters
+    ----------
+    point1 :
+        first point to be considered
+    point2 :
+        second point to be considered
+    dimension :
+        number of predictors/dimension of the points
+
+    Returns
+    -------
+    int
+        the euclidean distance between the points
+
+    """
     distance=0
     for x in range(dimension):
         distance += pow((point1[x] - point2[x]), 2)
     return math.sqrt(distance)
 
 def getNeighbors(trainingSet, testInstance, distances):
+    """Short summary.
+        get a list of tuples, containing the data point and the distance from the testInstance
+    Parameters
+    ----------
+    trainingSet : type
+        training set of the implementation
+    testInstance : type
+        data point from which the distances will be calcualted
+    distances : type
+        list to which the tuples will be appeded
+
+    Returns
+    -------
+    list
+        list of tuples
+
+    """
     # Empty list to store distances of between testInstance and each trainSet item
     # Number of dimensions to check
     length=len(testInstance) - 1
@@ -23,6 +58,19 @@ def getNeighbors(trainingSet, testInstance, distances):
 
 
 def getResponse(neighbors):
+    """Short summary.
+        return the majority vote given the neighbors
+    Parameters
+    ----------
+    neighbors : list
+        list of neighbors to be considered
+
+    Returns
+    -------
+    bool
+        the majority, either true or false
+
+    """
     # Empty dictionary to hold each output and the counts
     count={True: 0, False: 0}
     # For each neighbors, retreive the class to which it belongs, and add to count
@@ -41,6 +89,19 @@ def getResponse(neighbors):
 
 
 def crossValidation(training):
+    """Short summary.
+        Run 5-fold cross validation
+    Parameters
+    ----------
+    training : type
+        Description of parameter `training`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     predictions = []
     for index in range(1, 6):
         print index
@@ -78,8 +139,8 @@ def crossValidation(training):
 
 def main():
     trainingOriginal = pd.read_csv(
-        filepath_or_buffer="~/Desktop/KNN Implmentation/adult.train.5fold.csv")
-    testOriginal = pd.read_csv(filepath_or_buffer="~/Desktop/KNN Implmentation/adult.test.csv")
+        filepath_or_buffer="~/Desktop/KNN Implementation/adult.train.5fold.csv")
+    testOriginal = pd.read_csv(filepath_or_buffer="~/Desktop/KNN Implementation/adult.test.csv")
 
     # Select only the numeric data
     training = pd.DataFrame(trainingOriginal.select_dtypes(['number']))
